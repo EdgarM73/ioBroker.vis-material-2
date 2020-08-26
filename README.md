735e21c1f0e16e5c0d11d474f2dae4211a711b8e
Erdal Akkaya <erdal@sm.fritz.box>
2020-08-26 08:24:42 +0200

    initial commit to material advanced repo

--- README.md
+++ README.md
@@ -1,2 +1,78 @@
-# ioBroker.vis-material-2
+![Logo](admin/material.png)
+
+# ioBroker.vis-material-advanced
 This Adapter provides standardized Widgets for vis in ioBroker. Lots of different predifined widgets :)
+============
+
+![Number of Installations](http://iobroker.live/badges/vis-material-advanced-installed.svg) ![Number of Installations](http://iobroker.live/badges/vis-material-advanced-stable.svg) [![NPM version](http://img.shields.io/npm/v/iobroker.vis-material-advanced.svg)](https://www.npmjs.com/package/iobroker.vis-material-advanced)
+[![Downloads](https://img.shields.io/npm/dm/iobroker.vis-material-advanced.svg)](https://www.npmjs.com/package/iobroker.vis-material-advanced)
+[![Travis-CI](http://img.shields.io/travis/nisiode/ioBroker.vis-material-advanced/master.svg)](https://travis-ci.org/nisiode/ioBroker.vis-material-advanced)
+
+[![NPM](https://nodei.co/npm/iobroker.vis-material-advanced.png?downloads=true)](https://nodei.co/npm/iobroker.vis-material-advanced/)
+
+
+material-advanced - Advanced Material widgets for ioBroker.vis.
+
+This Adapter has been originally forked from ioBroker.vis-material started by nisio.
+
+Due to very huge changes and no response from my puu Request ( https://github.com/iobroker-community-adapters/ioBroker.vis-material/pull/18 ) and still lots of Ideas for this Adapter I decided to create a new Adapter to enable delivery to the latest Repository.
+
+I have kept the version numbers created in the fork to show the changes made in past. ( also thanks to pix ).
+
+You can read instructions in material.js, material.html files
+
+These widgets are in addition to the material design style of Uhula, which you can find here:
+https://github.com/Uhula/ioBroker-Material-Design-Style
+
+
+![Example](img/widgets.png)
+
+
+## Changelog
+
+### 0.4.0
+- (EdgarM73) added colorize Effect to Temperature and Humidity Widget and option to define the values when to colorize
+             created new adapter to be independently from pull requests
+
+### 0.3.0
+- (EdgarM73) WindowShutter Widget has been re-created due to unknown error. Old widget is most likey not working anymore.
+             New Color Temperature Widget ( Kelvin ) has been created
+             new widgets placed are now standard 350px instead of 100% which made no sense ;)
+             minor bugfixes and refactoring
+             
+### 0.2.0
+- (EdgarM73) widgets are now customizable, text-color, icon, color of overlay
+
+### 0.1.8
+- (EdgarM73) renamed css classes due to incompatibility with Uhula Material Design Schema
+
+### 0.1.7
+- (EdgarM73) dark material button alike
+
+### 0.1.6 (2020-07-12)
+- (EdgarM73) feat: new Occupancy widget
+
+### 0.1.5 (2018-07-11)
+- (pix) feat: new window shutter widget
+
+### 0.1.4 (2018-07-10)
+- (pix) feat: new humidity widget
+
+### 0.1.3 (2018-01-21)
+- (nisio) feat: new dimmer widget
+
+### 0.1.2 (2018-01-20)
+- (nisio) feat: switch added to light widget
+
+### 0.1.1 (2018-01-14)
+- (nisio) feat: temperature and light widget
+
+### 0.1.0 (2018-01-13)
+- (nisio) Initial version for public testing (includes css from material design styles V1.8)
+
+### 0.0.1 (2018-01-01)
+- (nisio) Initial version for internal testing
+
+## License
+ Copyright (c) 2018-2018 nisiode
+ MIT
---
+++ widgets/material/js/material.js
@@ -0,0 +1,549 @@
+/*
+    ioBroker.vis material Widget-Set
+    version: "0.4.0"
+    Copyright 2018 nisiode<nisio.air@mail.com>
+    forked by Pix 7/2018 (humidity, shutter)
+    forked by EdgarM73 07/2020 ( occupancy Advanced )
+*/
+"use strict";
+
+// add translations for edit mode
+if (vis.editMode) {
+    $.extend(true, systemDictionary, {
+        "title": {
+            "en": "Title",
+            "de": "Titel",
+            "ru": "Заголовок"
+        },
+        "subtitle": {
+            "en": "Subtitle",
+            "de": "Untertitel",
+            "ru": "Подзаголовок"
+        }
+    });
+}
+
+// add translations for non-edit mode
+$.extend(true, systemDictionary, {
+    "Instance": {
+        "en": "Instance",
+        "de": "Instanz",
+        "ru": "Инстанция"
+    },
+    "open": {
+        "en": "open",
+        "de": "offen",
+        "ru": "открыто"
+    },
+    "tilted": {
+        "en": "tilted",
+        "de": "gekippt",
+        "ru": "приоткрыто"
+    },
+    "closed": {
+        "en": "closed",
+        "de": "zu",
+        "ru": "закрыто"
+    },
+    "on": {
+        "en": "on",
+        "de": "an",
+        "ru": "вкл"
+    },
+    "off": {
+        "en": "off",
+        "de": "aus",
+        "ru": "выкл"
+    },
+    "motion": {
+        "en": "motion",
+        "de": "Bewegung",
+        "ru": "motion"
+    },
+    "nomotion": {
+        "en": "no motion",
+        "de": "Nein",
+        "ru": "na"
+    },
+    "Text-Color": {
+        "en": "Text Color",
+        "de": "Textfarbe",
+        "ru": "na"
+    },
+    "opac-white": {
+        "en": "white opacity",
+        "de": "Transparenz Weiss",
+        "ru": "na"
+    },
+    "opac-red": {
+        "en": "Red opacity",
+        "de": "Transparenz Rot",
+        "ru": "na"
+    },
+    "opac-blue": {
+        "en": "blue opacity",
+        "de": "Transparenz Blau",
+        "ru": "na"
+    },
+    "opac-purple": {
+        "en": "purple opacity",
+        "de": "Transparenz Lila",
+        "ru": "na"
+    },
+    "opac-green": {
+        "en": "green opacity",
+        "de": "Transparenz Grün",
+        "ru": "na"
+    },
+    "opacity-color": {
+        "en": "opacity color",
+        "de": "Transparenz Farbe",
+        "ru": "na"
+    },
+    "colorizeByTemp": {
+        "en": "colorize By Temp",
+        "de": "einfärben durch Temp",
+        "ru": "раскрасить по температуре",
+        "pt": "colorir por Temp",
+        "nl": "inkleuren door temp",
+        "fr": "coloriser par température",
+        "it": "colorize By Temp",
+        "es": "colorear por temperatura",
+        "pl": "koloruj według temp",
+        "zh-cn": "通过临时着色"
+    },
+    "below": {
+        "en": "below",
+        "de": "niedriger",
+        "ru": "ниже",
+        "pt": "abaixo",
+        "nl": "hieronder",
+        "fr": "au dessous de",
+        "it": "sotto",
+        "es": "abajo",
+        "pl": "poniżej",
+        "zh-cn": "下面"
+    },
+    "normal": {
+        "en": "normal",
+        "de": "normal",
+        "ru": "нормальный",
+        "pt": "normal",
+        "nl": "normaal",
+        "fr": "Ordinaire",
+        "it": "normale",
+        "es": "normal",
+        "pl": "normalna",
+        "zh-cn": "正常"
+    },
+    "above": {
+        "en": "above",
+        "de": "über",
+        "ru": "выше",
+        "pt": "acima",
+        "nl": "bovenstaande",
+        "fr": "au dessus",
+        "it": "sopra",
+        "es": "encima",
+        "pl": "powyżej",
+        "zh-cn": "以上"
+    }
+});
+
+vis.binds.material = {
+    version: "0.3.0",
+    showVersion: function() {
+        if (vis.binds.material.version) {
+            console.log('Version material: ' + vis.binds.material.version);
+            vis.binds.material.version = null;
+        }
+    },
+    tplMdListDoor: function(widgetID, view, data) {
+        const srcOpen = 'widgets/material/img/fts_door_open.png';
+        const srcClosed = 'widgets/material/img/fts_door.png';
+        const valOpen = _('open');
+        const valClosed = _('closed');
+
+        var $div = $('#' + widgetID);
+        // if nothing found => wait
+        if (!$div.length) {
+            return setTimeout(function() {
+                vis.binds.material.tplMdListDoor(widgetID, view, data);
+            }, 100);
+        }
+
+        function update(state) {
+            var value = (state) ? valOpen : valClosed;
+            var src = (state) ? srcOpen : srcClosed;
+            $div.find('.md-list-value').html(value);
+            $div.find('.md-list-icon').find('img').attr('src', src);
+        }
+
+        if (data.oid) {
+            // subscribe on updates of value
+            vis.states.bind(data.oid + '.val', function(e, newVal, oldVal) {
+                update(newVal);
+            });
+
+            // set current value
+            update(vis.states[data.oid + '.val']);
+        }
+    },
+    tplMdListWindow: function(widgetID, view, data) {
+        const srcOpen = 'widgets/material/img/fts_window_2w_open.png';
+        const srcClosed = 'widgets/material/img/fts_window_2w.png';
+        const valOpen = _('open');
+        const valClosed = _('closed');
+        var $div = $('#' + widgetID);
+
+        // if nothing found => wait
+        if (!$div.length) {
+            return setTimeout(function() {
+                vis.binds.material.tplMdListWindow(widgetID, view, data);
+            }, 100);
+        }
+
+        function update(state) {
+            var value = (state) ? valOpen : valClosed;
+            var src = (state) ? srcOpen : srcClosed;
+            $div.find('.md-list-value').html(value);
+            $div.find('.md-list-icon').find('img').attr('src', src);
+        }
+
+        if (data.oid) {
+            // subscribe on updates of value
+            vis.states.bind(data.oid + '.val', function(e, newVal, oldVal) {
+                update(newVal);
+            });
+
+            // set current value
+            update(vis.states[data.oid + '.val']);
+        }
+    },
+    tplMdListTemp: function(widgetID, view, data) {
+        var $div = $('#' + widgetID);
+        const $colorize = data.attr('colorizeByTemp');
+        const $low = data.attr('below');
+        const $normal = data.attr('normal');
+        const $high = data.attr('above');
+        const $original_class = data.attr('opacity-color');
+
+        // if nothing found => wait
+        if (!$div.length) {
+            return setTimeout(function() {
+                vis.binds.material.tplMdListTemp(widgetID, view, data);
+            }, 100);
+        }
+
+        // grey out the value in case the last change is more than 24h ago
+        var curTime = new Date().getTime();
+        var lcTime = vis.states[data.oid + '.lc'];
+        var seconds = (curTime - lcTime) / 1000;
+        if (seconds > 86400) {
+            $div.find('.md-list-value').css('opacity', '0.5');
+        }
+
+        function update(state) {
+            if (typeof state === 'number') {
+                $div.find('.md-list-value').html(state.toFixed(1) + ' °C');
+            }
+            if ($colorize == true) {
+                if (state <= $low) {
+                    console.log('Temperatur ist niedrig ');
+                    $div.find('.overlay').removeClass('opac-white opac-green opac-purple opac-red opac-blue opac-below opac-above').addClass('opac-below');
+                } else if (state > $normal && state < $high) {
+                    $div.find('.overlay').removeClass('opac-white opac-green opac-purple opac-red opac-blue opac-below opac-above').addClass($original_class);
+                } else if (state >= $high) {
+                    console.log('Temperatur ist hoch');
+                    $div.find('.overlay').removeClass('opac-white opac-green opac-purple opac-red opac-blue opac-below opac-above').addClass('opac-above');
+                } else {
+                    console.log('Fehler in Temperatur berechnung');
+                }
+            }
+
+        }
+
+        if (data.oid) {
+            // subscribe on updates of value
+            vis.states.bind(data.oid + '.val', function(e, newVal, oldVal) {
+                update(newVal);
+            });
+
+            // set current value
+            update(vis.states[data.oid + '.val']);
+        }
+    },
+    tplMdListHumid: function(widgetID, view, data) {
+        var $div = $('#' + widgetID);
+        const $colorize = data.attr('colorizeByTemp');
+        const $low = data.attr('below');
+        const $normal = data.attr('normal');
+        const $high = data.attr('above');
+        const $original_class = data.attr('opacity-color');
+        // if nothing found => wait
+        if (!$div.length) {
+            return setTimeout(function() {
+                vis.binds.material.tplMdListHumid(widgetID, view, data);
+            }, 100);
+        }
+
+        // grey out the value in case the last change is more than 24h ago
+        var curTime = new Date().getTime();
+        var lcTime = vis.states[data.oid + '.lc'];
+        var seconds = (curTime - lcTime) / 1000;
+        if (seconds > 86400) {
+            $div.find('.md-list-value').css('opacity', '0.5');
+        }
+
+        function update(state) {
+            if (typeof state === 'number') {
+                $div.find('.md-list-value').html(state.toFixed(1) + ' %');
+            }
+            if ($colorize == true) {
+                if (state <= $low) {
+                    console.log('Temperatur ist niedrig ');
+                    $div.find('.overlay').removeClass('opac-white opac-green opac-purple opac-red opac-blue opac-below opac-above').addClass('opac-below');
+                } else if (state > $normal && state < $high) {
+                    $div.find('.overlay').removeClass('opac-white opac-green opac-purple opac-red opac-blue opac-below opac-above').addClass($original_class);
+                } else if (state >= $high) {
+                    console.log('Temperatur ist hoch');
+                    $div.find('.overlay').removeClass('opac-white opac-green opac-purple opac-red opac-blue opac-below opac-above').addClass('opac-above');
+                } else {
+                    console.log('Fehler in Temperatur berechnung');
+                }
+            }
+        }
+
+        if (data.oid) {
+            // subscribe on updates of value
+            vis.states.bind(data.oid + '.val', function(e, newVal, oldVal) {
+                update(newVal);
+            });
+
+            // set current value 
+            update(vis.states[data.oid + '.val']);
+        }
+    },
+    tplMdListOccupancy: function(widgetID, view, data) {
+        const srcMotion = 'widgets/material/img/motion.png';
+        const srcNoMotion = 'widgets/material/img/no-motion.png';
+        const valMotion = _('motion');
+        const valNoMotion = _('nomotion');
+
+        var $div = $('#' + widgetID);
+
+        // if nothing found => wait
+        if (!$div.length) {
+            return setTimeout(function() {
+                vis.binds.material.tplMdListOccupancy(widgetID, view, data);
+            }, 100);
+        }
+
+        // grey out the value in case the last change is more than 24h ago
+        var curTime = new Date().getTime();
+        var lcTime = vis.states[data.oid + '.lc'];
+        var seconds = (curTime - lcTime) / 1000;
+        if (seconds > 86400) {
+            $div.find('.md-list-value').css('opacity', '0.5');
+        }
+
+        function update(state) {
+            var value = (state) ? valMotion : valNoMotion;
+            var src = (state) ? srcMotion : srcNoMotion;
+            $div.find('.md-list-value').html(value);
+            $div.find('.md-list-icon').find('img').attr('src', src);
+
+        }
+
+        if (data.oid) {
+            // subscribe on updates of value
+            vis.states.bind(data.oid + '.val', function(e, newVal, oldVal) {
+                update(newVal);
+            });
+
+            // set current value
+            update(vis.states[data.oid + '.val']);
+        }
+    },
+    tplMdListLight: function(widgetID, view, data) {
+        const srcOff = 'widgets/material/img/light_light_dim_00.png';
+        const srcOn = 'widgets/material/img/light_light_dim_100.png';
+        var $div = $('#' + widgetID);
+
+        // if nothing found => wait
+        if (!$div.length) {
+            return setTimeout(function() {
+                vis.binds.material.tplMdListLight(widgetID, view, data);
+            }, 100);
+        }
+
+        function update(state) {
+            var src = (state) ? srcOn : srcOff;
+            var $tmp = $('#' + widgetID + '_checkbox');
+            $tmp.prop('checked', state);
+            $div.find('.md-list-icon').find('img').attr('src', src);
+        }
+
+        if (!vis.editMode) {
+            var $this = $('#' + widgetID + '_checkbox');
+            $this.change(function() {
+                var $this_ = $(this);
+                vis.setValue($this_.data('oid'), $this_.prop('checked'));
+            });
+        }
+
+        if (data.oid) {
+            // subscribe on updates of value
+            vis.states.bind(data.oid + '.val', function(e, newVal, oldVal) {
+                update(newVal);
+            });
+
+            // set current value
+            update(vis.states[data.oid + '.val']);
+        }
+    },
+    tplMdListWindowShutter: function(widgetID, view, data) {
+        const srcOff = 'widgets/material/img/light_light_dim_00.png';
+        const srcOn = 'widgets/material/img/light_light_dim_100.png';
+        var $div = $('#' + widgetID);
+
+        // if nothing found => wait
+        if (!$div.length) {
+            return setTimeout(function() {
+                vis.binds.material.tplMdListWindowShutter(widgetID, view, data);
+            }, 100);
+        }
+
+        function update(state) {
+            var percent = Math.ceil(state / 10);
+            var name;
+
+            console.log("Status: " + state);
+
+            if (data.attr('inverted') == true) {
+
+                name = 10 - parseInt(percent);
+                console.log('Inverted -> name: ' + name);
+            } else {
+                name = percent;
+            }
+
+            var src = 'widgets/material/img/fts_shutter_' + name + '0.png';
+            console.log(' name : ' + name + " Icon : " + src);
+            $div.find('.md-list-icon').find('img').attr('src', src);
+        }
+
+        /* if (!vis.editMode) {
+            var $this = $('#' + widgetID + '_slider');
+            $this.change(function () {
+                var $this_ = $(this);
+                vis.setValue($this_.data('oid'), $this_.prop('checked'));
+            });
+        } */
+
+        if (data.oid) {
+            // subscribe on updates of value
+            vis.states.bind(data.oid + '.val', function(e, newVal, oldVal) {
+                update(newVal);
+            });
+
+            // set current value
+            update(vis.states[data.oid + '.val']);
+        }
+    },
+    tplMdListLightDim: function(widgetID, view, data) {
+        const srcOff = 'widgets/material/img/light_light_dim_00.png';
+        const srcOn = 'widgets/material/img/light_light_dim_100.png';
+        var $div = $('#' + widgetID);
+
+        // if nothing found => wait
+        if (!$div.length) {
+            return setTimeout(function() {
+                vis.binds.material.tplMdListLightDim(widgetID, view, data);
+            }, 100);
+        }
+
+        function update(state) {
+
+            var src = 'widgets/material/img/light_light_dim_' + Math.ceil(state / 10) + '0.png';
+            $div.find('.md-list-icon').find('img').attr('src', src);
+        }
+
+        /* if (!vis.editMode) {
+            var $this = $('#' + widgetID + '_slider');
+            $this.change(function () {
+                var $this_ = $(this);
+                vis.setValue($this_.data('oid'), $this_.prop('checked'));
+            });
+        } */
+
+        if (data.oid) {
+            // subscribe on updates of value
+            vis.states.bind(data.oid + '.val', function(e, newVal, oldVal) {
+                update(newVal);
+            });
+
+            // set current value
+            update(vis.states[data.oid + '.val']);
+        }
+    },
+    tplMdListLightKelvin: function(widgetID, view, data) {
+        const srcCold = 'widgets/material/img/fts_kelvin_kaltweiss.png';
+        const srcMedium = 'widgets/material/img/fts_kelvin_mittel.png';
+        const srcWarm = 'widgets/material/img/fts_kelvin_warmweiss.png';
+
+        var $div = $('#' + widgetID);
+        console.log('LightKelvin called');
+        // if nothing found => wait
+        if (!$div.length) {
+            return setTimeout(function() {
+                vis.binds.material.tplMdListLightKelvin(widgetID, view, data);
+            }, 100);
+        }
+
+        function update(state) {
+            var drittel = (data.attr('max') - data.attr('min')) / 3;
+            /*console.log("ein Drittel ist: " + drittel + " von  :" + data.attr('max'));
+            console.log("Minimum : " + data.attr('min'));
+            console.log('Maximum : ' + data.attr('max'));
+            console.log('state : ' + state);*/
+            var medium = parseInt(data.attr('min')) + parseInt(drittel);
+            var cold = parseInt(data.attr('max')) - parseInt(drittel);
+            var src;
+            if (state >= data.attr('min') && state < medium) {
+                console.log('warmweiss : min -> ' + data.attr('min') + " state -> " + state + " medium ->" + medium);
+                src = srcWarm;
+            } else if (state >= medium && state < cold) {
+                console.log('medium: min -> ' + data.attr('min') + " state -> " + state + " medium ->" + medium);
+                src = srcMedium;
+            } else if (state >= cold && state <= data.attr('max')) {
+                console.log('kaltweiss: max -> ' + data.attr('max') + " state -> " + state + " cold ->" + cold);
+                src = srcCold;
+            } else {
+                console.log('Fehler');
+                src = 'Fehler';
+            }
+            $div.find('.md-list-icon').find('img').attr('src', src);
+        }
+
+        /* if (!vis.editMode) {
+            var $this = $('#' + widgetID + '_slider');
+            $this.change(function () {
+                var $this_ = $(this);
+                vis.setValue($this_.data('oid'), $this_.prop('checked'));
+            });
+        } */
+
+        if (data.oid) {
+            // subscribe on updates of value
+            vis.states.bind(data.oid + '.val', function(e, newVal, oldVal) {
+                update(newVal);
+            });
+
+            // set current value
+            update(vis.states[data.oid + '.val']);
+        }
+    }
+};
+
+vis.binds.material.showVersion();
